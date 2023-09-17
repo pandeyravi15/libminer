f3b767e27d2727b9e3eef6a8630dc567801ddf16
pandeyravi15 <vishalintelligent.com>
2023-09-17 14:23:06 -0500

    added test

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -12,7 +12,7 @@ lib_summary <- function(){
 
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[,"LibPath"])
-  pkg_df <- as.data.frame(pkg_tbl,stringAsFactors =FALSE)
+  pkg_df <- as.data.frame(pkg_tbl,stringAsFactors = FALSE)
   names(pkg_df) <- c("Library","n_packages")
   pkg_df
 }
