275a72bae45ae79152ba770d02c3f63f2af50704
pandeyravi15 <vishalintelligent.com>
2023-09-17 16:13:52 -0500

    add size param to lib_summary to calculate lib size

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,17 +2,31 @@
 #'
 #'Provide a brief summary of the package libraries on your machine
 #'
-#' @return A data.frame containing the count of packages in each of the user's libraries
+#'@param sizes Should the sizes of the libraries be calculated?
+#'Logical; default `FALSE`.
+#'
+#' @return A data.frame containing the count of packages in each of the user's libraries.
+#'  A `lib_size` column is included if `sizes = TRUE`.
 #' @export
 #'
 #' @examples
 #' lib_summary()
+#' lib_summary(sizes = TRUE)
 
-lib_summary <- function(){
-
+lib_summary <- function(sizes = FALSE){
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[,"LibPath"])
-  pkg_df <- as.data.frame(pkg_tbl,stringAsFactors = FALSE)
+  pkg_df <- as.data.frame(pkg_tbl,stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library","n_packages")
+
+  if(sizes) {
+    pkg_df$lib_size <- sapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      }
+    )
+  }
+
   pkg_df
 }
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -3,10 +3,16 @@ test_that("lib_summary returns expected results", {
   expect_s3_class(res, "data.frame")
   expect_equal(ncol(res), 2)
   expect_equal(names(res), c("Library", "n_packages"))
-  expect_type(res$Library, "integer")
+  expect_type(res$Library, "character")
   expect_type(res$n_packages, "integer")
 })
 
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
+  expect_error(lib_summary(sizes = "foo"))
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
 })
